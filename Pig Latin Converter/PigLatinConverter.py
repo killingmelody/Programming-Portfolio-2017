95e66e2776e2ca9ae59561cf43f93c5707eb75c7
Dylan Webb <pollywolly237@gmail.com>
2017-05-24 12:34:57 -0600

    Add files via upload

---
+++ Pig Latin Converter/PigLatinConverter.py
@@ -0,0 +1,19 @@
+#PigLatinConverter
+#Written by Dylan Webb on 1.27.17
+
+vowels = "aeiouAEIOU"
+
+firstName = raw_input('Type your first name and hit enter: ')
+lastName = raw_input('Do the same for your last name: ')
+
+if firstName[0] in vowels : #check for starting vowel in first name
+    firstName += "way"
+else:
+    firstName = firstName[1:] + firstName[0] + "ay"
+    
+if lastName[0] in vowels : #check for starting vowel in last name
+    lastName += "way"
+else:
+    lastName = lastName[1:] + lastName[0] + "ay"
+
+print "\nYour pig latin name is: " + firstName.capitalize() + ", " + lastName.capitalize()
