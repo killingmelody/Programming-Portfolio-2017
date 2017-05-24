2508c26df728f59532499d857040280e7e7419a9
Dylan Webb <pollywolly237@gmail.com>
2017-05-24 12:35:25 -0600

    Add files via upload

---
+++ 99 Bottles/DW99bottles.py
@@ -0,0 +1,16 @@
+#99 bottles code
+
+i = 99
+while i > 0:
+    if i == 1 :
+        print i, "bottle of an undefined beverage on the wall"
+        print i, "bottle of an undefined beverage"
+    else :
+        print i, "bottles of an undefined beverage on the wall"
+        print i, "bottles of an undefined beverage"
+    print "Take one down & pass it around"
+    i -= 1
+    if i == 1 :
+        print i, "bottle of an undefined beverage on the wall\n"
+    else :
+        print i, "bottles of an undefined beverage on the wall\n"
