477c1c4d52c50c1f5997cf145164704ae584eac5
Dylan Webb <pollywolly237@gmail.com>
2017-05-24 17:22:12 -0600

    Update DW99bottles.py

--- 99 Bottles/DW99bottles.py
+++ 99 Bottles/DW99bottles.py
@@ -3,14 +3,14 @@
 i = 99
 while i > 0:
     if i == 1 :
-        print i, "bottle of an undefined beverage on the wall"
-        print i, "bottle of an undefined beverage"
+        print(i, "bottle of an undefined beverage on the wall")
+        print(i, "bottle of an undefined beverage")
     else :
-        print i, "bottles of an undefined beverage on the wall"
-        print i, "bottles of an undefined beverage"
-    print "Take one down & pass it around"
+        print(i, "bottles of an undefined beverage on the wall")
+        print(i, "bottles of an undefined beverage")
+    print("Take one down & pass it around")
     i -= 1
     if i == 1 :
-        print i, "bottle of an undefined beverage on the wall\n"
+        print(i, "bottle of an undefined beverage on the wall\n")
     else :
-        print i, "bottles of an undefined beverage on the wall\n"
+        print(i, "bottles of an undefined beverage on the wall\n")
